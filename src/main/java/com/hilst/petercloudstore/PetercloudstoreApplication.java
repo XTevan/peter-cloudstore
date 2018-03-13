df27acc74547309a596143b548be2ea61d2eef02
Estevão Hilst <xtevan@gmail.com>
2018-03-13 14:28:10 -0300

    Created project infrastructure

---
+++ src/main/java/com/hilst/petercloudstore/PetercloudstoreApplication.java
@@ -0,0 +1,12 @@
+package com.hilst.petercloudstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetercloudstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetercloudstoreApplication.class, args);
+	}
+}
